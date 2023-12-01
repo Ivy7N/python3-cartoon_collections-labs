133faf50d74ed552596b64ac9ded9c0e5827a3b2
your_github_username <your_github_email>
2023-12-01 10:11:02 +0300

    collections

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -12,17 +12,29 @@ print(result)
 
 def long_planeteer_calls(calls, length_threshold):
     return any(len(call) > length_threshold for call in calls)
-calls = ["earth", "fire", "wind", "water"]
-result = long_planeteer_calls(calls, 5)
-print(result)
+
+short_words = ["puff", "go", "two"]
+result_short = long_planeteer_calls(short_words, 3)
+print(result_short)
+
+assorted_words = ["two", "go", "industrious", "bop"]
+result_assorted = long_planeteer_calls(assorted_words, 5)
+print(result_assorted)
 
 def find_the_cheese(items, cheeses):
     for item in items:
         if item in cheeses:
             return item
-    return None  
+    return None
 
-items = ["crackers", "cheddar", "apple", "swiss"]
-cheeses = ["cheddar", "swiss", "gouda"]
-result = find_the_cheese(items, cheeses)
-print(result)
+snacks = ["crackers", "gouda", "thyme"]
+result_snacks = find_the_cheese(snacks, ["gouda", "cheddar", "swiss"])
+print(result_snacks) 
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+result_soup = find_the_cheese(soup, ["gouda", "cheddar", "swiss"])
+print(result_soup) 
+
+ingredients = ["garlic", "rosemary", "bread"]
+result_ingredients = find_the_cheese(ingredients, ["gouda", "cheddar", "swiss"])
+print(result_ingredients)  
